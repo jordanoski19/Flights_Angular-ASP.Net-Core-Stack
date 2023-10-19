01c175bb1970b8acb7da0d8a5808ed5fd4358a65
jordanoski19 <102685979+jordanoski19@users.noreply.github.com>
2023-10-19 20:23:12 +0200

    Search Flights Query

--- Flights/ClientApp/src/app/search-flights/search-flights.component.ts
+++ Flights/ClientApp/src/app/search-flights/search-flights.component.ts
@@ -2,6 +2,7 @@ import { Time } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FlightService } from './../api/services/flight.service';
 import { FlightRm } from '../api/models';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-search-flights',
@@ -13,23 +14,25 @@ export class SearchFlightsComponent implements OnInit {
 
   searchResult: FlightRm[] = []
 
-  constructor(private flightService: FlightService) {
-  }
+  constructor(private flightService: FlightService,
+    private fb: FormBuilder) { }
+
+  searchForm = this.fb.group({
+    from: [''],
+    destination: [''],
+    fromDate: [''],
+    toDate: [''],
+    numberOfPassengers: [1]
+  })
 
   ngOnInit(): void {
+    this.search();
   }
 
   search() {
-    this.flightService.searchFlight({}).subscribe(
-      (response: FlightRm[]) => {
-        console.log('Flight Data:', response);
-        this.searchResult = response;
-      },
-      (error) => {
-        console.error('Error:', error);
-        this.handleError(error);
-      }
-    );
+    this.flightService.searchFlight(this.searchForm.value)
+      .subscribe(response => this.searchResult = response,
+        this.handleError)
   }
 
   private handleError(err: any) {
